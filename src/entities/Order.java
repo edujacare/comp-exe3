ea0e020c607a1ef090ecb74822112e1550c53368
Eduardo R Andrade <edujacare@hotmail.com>
2022-02-17 08:57:27 -0300

    Criação do programa principal

--- src/entities/Order.java
+++ src/entities/Order.java
@@ -76,6 +76,27 @@ public class Order {
 		return sum;
 	}
 	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Order moment: ");
+		sb.append(sdf.format(moment) + "\n");
+		sb.append("Order status: ");
+		sb.append(status + "\n");
+		sb.append("Client: ");
+		sb.append(client + "\n");
+		sb.append("Order items:\n");
+		for (OrderItem item : items) {
+			sb.append(item + "\n");
+		}
+		sb.append("Total price: $ ");
+		sb.append(String.format("%.2f", total()));
+		return sb.toString();
+		
+		
+			
+		
+	}
+	
 	
 	
 	
